aaa0a9bda4784abe2c73ab244b1933d6dec4d754
Yevheniia Smolanka <yevheniia.smolanka@actum.cz>
2018-11-05 10:07:29 +0100

    add files for PR

---
+++ src/components/BeerList.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import BeerItem from "./BeerItem";
+import "../styles/BeerList.scss";
+
+class BeerList extends Component {
+  render() {
+    return (
+      <div className="beerContainer">
+        <ul className="d-flex jusctify-content-between list-unstyled row mt-2">
+        {this.props.results.map(item => (
+          <BeerItem
+            key={item.name} 
+            {...item}
+          />
+        ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+BeerList.defaultProps = {
+  name: ''
+};
+
+BeerList.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string
+  }))
+}
+
+export default BeerList;
